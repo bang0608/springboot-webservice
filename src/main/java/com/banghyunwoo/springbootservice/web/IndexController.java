2e62ee3783f6ed6c09b26cf1b7110d8cb09827f7
Hyunwoo_Bang <b4409@naver.com>
2021-04-21 15:46:39 +0900

    OAuth 2.0 Google/Naver Login 구현

--- src/main/java/com/banghyunwoo/springbootservice/web/IndexController.java
+++ src/main/java/com/banghyunwoo/springbootservice/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.banghyunwoo.springbootservice.web;
 
+import com.banghyunwoo.springbootservice.config.auth.LoginUser;
+import com.banghyunwoo.springbootservice.config.auth.dto.SessionUser;
+import com.banghyunwoo.springbootservice.web.domain.user.User;
 import com.banghyunwoo.springbootservice.web.dto.PostsResponseDto;
 import com.banghyunwoo.springbootservice.web.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +11,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String getIndexPage(Model model){
+    public String getIndexPage(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+//        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        // @@LoginUser 애노테이션 생성 -> 파라미터 처리
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
--- src/test/java/com/banghyunwoo/springbootservice/web/IndexControllerTest.java
+++ src/test/java/com/banghyunwoo/springbootservice/web/IndexControllerTest.java
@@ -1,13 +1,27 @@
 package com.banghyunwoo.springbootservice.web;
 
+import com.banghyunwoo.springbootservice.web.dto.PostsUpdateRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,13 +30,28 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
-    public void 메인페이지_로딩(){
+    public void 메인페이지_로딩() throws Exception {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트 웹 서비스");
-
+//        assertThat(body).contains("스프링 부트 웹 서비스");
+        mvc.perform(get("/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
     }
 }
