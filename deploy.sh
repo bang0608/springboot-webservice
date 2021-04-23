78f4ebee9c4231b600863e4582240a690709c0dc
Ubuntu <ubuntu@webservice.banghyunwoo.com>
2021-04-23 23:39:47 +0900

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/dev
+PROJECT_NAME=springboot-webservice
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull"
+
+git pull
+
+echo "> Start Project Build"
+
+./gradlew build
+
+echo "> Move dev Directory"
+
+cd $REPOSITORY
+
+echo "> Copy Build Fild"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "CURRENT_PID" ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
