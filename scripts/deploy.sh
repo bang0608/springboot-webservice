adcdbcc28a30e39e28243857573f891380eebabe
Hyunwoo_Bang <b4409@naver.com>
2021-05-10 18:14:40 +0900

    deployt test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,16 +9,14 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-webservice | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-    mkdir -p testDirectory
 else
     echo "> kill -15 $CURRENT_PID"
-    mkdir -p testDirectoryKill
     kill -15 $CURRENT_PID
     sleep 5
 fi
